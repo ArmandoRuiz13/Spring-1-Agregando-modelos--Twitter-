3de1f9cd8de7e02aef8f8a7bb060049fb66ce57c
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-25 17:53:43 -0500

    18- Agregando archivos y prueba para sprint 3 requerimiento 1

---
+++ test/views/UserView.test.js
@@ -0,0 +1,12 @@
+const UserView = require("./../../app/views/UserView")
+
+describe("Test para UserView sprint 3"),() => {
+    test("Requerimiento 1- retornar un error objecto, al intentar crear un nuevo usuario
+    con with an null payload", ( =>{
+        const payload = null
+        const result = UserView.createUser(payload)
+        // https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/payload no existe/)
+
+    })
+)}
