3d965839f26b3e7be485b425103d4adf966bd7fa
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 22:51:23 -0500

    7- Haciendo funcionar la segunda prueba con funcion new date

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,19 +3,17 @@ const User= require('./../../app/models/User')
 
 describe('Pruebas User JS ',()=> {
     test('Requerimiento 1: Prueba objetos para la clase User', () =>{
-        const user = new User(1,"Armandobolt13","Armando","Bio", "dateCreated","lastUpdate")
+        const user = new User(1,"Armandobolt13","Armando","Bio")
         // valores esperados
         expect(user.id).toBe(1)
         expect(user.username).toBe("Armandobolt13")
         expect(user.name).toBe("Armando")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdate).toBe("lastUpdate")
     })
     test('Requerimiento 2: Fechas en atributos de user', () =>{
-        const user = new User(1,"Armandobolt13","Armando","Bio", "dateCreated","lastUpdate")
+        const user = new User(1,"Armandobolt13","Armando","Bio")
         // valores esperados
-        expect(user.dateCreated).not.toBeUndefined()("dateCreated") //Verifica que el valor no sea undefined
-        expect(user.lastUpdate).not.toBeUndefined()("lastUpdate")
+        expect(user.dateCreated).not.toBeUndefined() //Verifica que el valor no sea undefined
+        expect(user.lastUpdate).not.toBeUndefined()
     })
 })
