08e9ac15c26031cce927dc76f78d5bfcbd4128b5
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 22:46:15 -0500

    6- Agregando segunda prueba con fechas en atributos user

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,8 +1,8 @@
 const { TestWatcher } = require('jest')
 const User= require('./../../app/models/User')
 
-describe('',()=> {
-    test('', () =>{
+describe('Pruebas User JS ',()=> {
+    test('Requerimiento 1: Prueba objetos para la clase User', () =>{
         const user = new User(1,"Armandobolt13","Armando","Bio", "dateCreated","lastUpdate")
         // valores esperados
         expect(user.id).toBe(1)
@@ -11,5 +11,11 @@ describe('',()=> {
         expect(user.bio).toBe("Bio")
         expect(user.dateCreated).toBe("dateCreated")
         expect(user.lastUpdate).toBe("lastUpdate")
+    })
+    test('Requerimiento 2: Fechas en atributos de user', () =>{
+        const user = new User(1,"Armandobolt13","Armando","Bio", "dateCreated","lastUpdate")
+        // valores esperados
+        expect(user.dateCreated).not.toBeUndefined()("dateCreated") //Verifica que el valor no sea undefined
+        expect(user.lastUpdate).not.toBeUndefined()("lastUpdate")
     })
 })
