a64b93133cb063b711a2c411d98ba79152d6948e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-25 16:20:51 -0500

    16- Funcionando requerimiento 2 con prueba, 3 pruebas hasta requerimiento 3 funcionando

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,11 +5,12 @@ class UserService{
         return new User(id,username,name, "Sin bio")
     }
     static getInfo(user){ // get nos ayuda a obtener informacion especifica
-        return user[user.id, user.username, user.name, user.bio]
+        return [user.id, user.username, user.name, user="Sin bio"]
     }
     static updateUserUsername ( user,newUsername){
         return user.username = newUsername
 
     }
+ 
 }
 module.exports = UserService
